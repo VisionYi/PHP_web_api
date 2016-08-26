005ce6dfa4b3080f2de210579dbb43e5e3ff0d41
ShianYi <scps950613@gmail.com>
2016-08-27 01:51:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @author: ShianYi
- * @version : 1.1
- * @dateTime: 2016-06-12 14:22:13
- * @description: 自製PHP MVC framework
+ * @version : 1.0.0 開始
+ * @dateTime: 2016-08-26 10:22:13
+ * @description: 自製PHP web_api
  */
 
 // 進入web_app資料夾
