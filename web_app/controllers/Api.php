c672f1e163f9f704e687bc3e753cb7128348358d
鹹魚Yi <scps950613@gmail.com>
2016-09-19 17:44:31 +0800

    加上前端 偵測錯誤header

--- web_app/controllers/Api.php
+++ web_app/controllers/Api.php
@@ -8,12 +8,16 @@ class Api extends DB_Api {
         require_once 'database/libs/MyPDO.php';
         $this->DB = new MyPDO();
         // $this->DB->debugDB_SQL = true;       //debug檢測SQL的語法錯誤
+
+        ob_start();
     }
 
     function __destruct() {
         $this->DB->closeDB();
         // $this->check_json_error_log();       //檢測當前Api印出的資訊 是否符合JSON格式
         // $this->check_json_error_header(500, "Internal Server Error!!");
+
+        // ob_end_clean();
         exit();
     }
 
